6bf187ad85fec421c4d1bf0a85415d5e9d08bada
Fandrioni Effendi <fandriony@gmail.com>
2021-05-27 09:09:22 +0700

    Update web_config.php

--- config/web_config.php
+++ config/web_config.php
@@ -1,132 +1,137 @@
 <?php
-
+        
 		return array (
   'name' => 'Aplikasi Buku Tamu',
   'broadcast_network' => false,
-  'tujuan_tamu' =>
+  'tujuan_tamu' => 
   array (
-    0 =>
+    0 => 
     array (
       'tag' => 'DIR-A',
       'name' => 'DIREKTORAT A',
     ),
-    1 =>
+    1 => 
     array (
       'tag' => 'DIR-B',
       'name' => 'DIREKTORAT B',
     ),
+    2 => 
+    array (
+      'tag' => 'DIR-C',
+      'name' => 'DIREKTORAT C',
+    ),
   ),
-  'kategori_tamu' =>
+  'kategori_tamu' => 
   array (
-    0 =>
+    0 => 
     array (
       'tag' => 'TNI',
       'name' => 'TNI',
     ),
-    1 =>
+    1 => 
     array (
       'tag' => 'POLISI',
       'name' => 'POLISI',
     ),
-    2 =>
+    2 => 
     array (
       'tag' => 'PEMERINTAHAN-DALAM-NEGERI',
       'name' => 'PEMERINTAHAN DALAM NEGERI',
     ),
-    3 =>
+    3 => 
     array (
       'tag' => 'PEMERINTAHAN-LUAR-NEGERI',
       'name' => 'PEMERINTAHAN LUAR NEGERI',
     ),
-    4 =>
+    4 => 
     array (
       'tag' => 'SIPIL',
       'name' => 'SIPIL',
     ),
   ),
-  'identity_list' =>
+  'identity_list' => 
   array (
-    0 =>
+    0 => 
     array (
       'tag' => 'KTP',
       'name' => 'KTP',
       'taxonomy' => 'TKP',
     ),
-    1 =>
+    1 => 
     array (
       'tag' => 'PASSPORT-WNA',
       'name' => 'PASSPORT-WNA',
       'taxonomy' => 'PASSPORT',
     ),
-    2 =>
+    2 => 
     array (
       'tag' => 'PASSPORT-WNI',
       'name' => 'PASSPORT-WNI',
       'taxonomy' => 'PASSPORT',
     ),
-    3 =>
+    3 => 
     array (
       'tag' => 'SIM-C',
       'name' => 'SIM C',
       'taxonomy' => 'SIM',
     ),
-    4 =>
+    4 => 
     array (
       'tag' => 'SIM-A',
       'name' => 'SIM A',
       'taxonomy' => 'SIM',
     ),
-    5 =>
+    5 => 
     array (
       'tag' => 'SIM-B1',
       'name' => 'SIM B1',
       'taxonomy' => 'SIM',
     ),
-    6 =>
+    6 => 
     array (
       'tag' => 'SIM-B2',
       'name' => 'SIM B2',
       'taxonomy' => 'SIM',
     ),
-    7 =>
+    7 => 
     array (
       'tag' => 'SIM-D',
       'name' => 'SIM D',
       'taxonomy' => 'SIM',
     ),
-    8 =>
+    8 => 
     array (
       'tag' => 'SIM-INTERNASIONAL',
       'name' => 'SIM INTERNASIONAL',
       'taxonomy' => 'SIM',
     ),
-    9 =>
+    9 => 
     array (
       'tag' => 'ID-PERUSAHAAN',
       'name' => 'ID PERUSAHAAM',
       'taxonomy' => 'LAINYA',
     ),
-    10 =>
+    10 => 
     array (
       'tag' => 'ID-MAHASISWA',
       'name' => 'ID MAHASISWA',
       'taxonomy' => 'LAINYA',
     ),
-    11 =>
+    11 => 
     array (
       'tag' => 'LAINYA',
       'name' => 'LAINYA',
       'taxonomy' => 'LAINYA',
     ),
   ),
-  1 =>
+  1 => 
   array (
-    0 =>
+    0 => 
     array (
       'tag' => 'BAGIAN-A-AASSA',
       'name' => 'yuan',
     ),
-    1 =>
+    1 => 
     array (
       'tag' => 'DIR-B',
       'name' => 'DIREKTORAT B',
